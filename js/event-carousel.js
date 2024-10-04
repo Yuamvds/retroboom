f4ab420fe0d518213927d2b0a9c6107d7b86ce4d
Yuam <arielvdsordonez@gmail.com>
2024-10-03 20:11:14 -0600

    .

---
+++ js/event-carousel.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
