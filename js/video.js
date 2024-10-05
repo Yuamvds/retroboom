f382f42c8b099542201e27d29ef8316b89fb164e
Yuam <arielvdsordonez@gmail.com>
2024-10-05 12:30:19 -0600

    .

--- js/video.js
+++
@@ -1,7 +0,0 @@
-const boton = document.getElementById('play-boton');
-const video = document.getElementById('video-subido');
-
-boton.addEventListener('click', () => {
-    video.src += "&autoplay=1";
-    boton.style.display = 'none';
-})
