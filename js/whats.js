9b04668b9521d5b6edf67f8d222f4ea0a7c50fad
Yuam <arielvdsordonez@gmail.com>
2024-10-06 16:51:53 -0600

    .

---
+++ js/whats.js
@@ -0,0 +1,10 @@
+const whatsButton = document.querySelectorAll('.whats');
+
+whatsButton.forEach(button => {
+    button.addEventListener('click', function(){
+        const product = this.getAttribute('data-product');
+        const whatsappLink = `https://api.whatsapp.com/send?phone=55705760&text=Hola, estoy interesado en ${encodeURIComponent(product)}`;
+
+        window.open(whatsappLink, '_blank');
+    });
+});
