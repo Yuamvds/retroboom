23a9703f1d63b4cd8ce3a17fb824bc5039a1bd83
Yuam <arielvdsordonez@gmail.com>
2024-10-21 14:01:53 -0600

    eventos completado

--- js/radio.js
+++ js/radio.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const playPause = document.getElementById("play");
     const playIcon = playPause.querySelector(".fi-sr-play");
     const pauseIcon = playPause.querySelector(".fi-sr-pause");
+    const currentSong = document.getElementById("currentSong");
   
     playPause.addEventListener("click", () => {
         if (audio.paused || audio.ended) {
@@ -17,4 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
             pauseIcon.classList.add("hide");
         }
     });
-  })
+
+    const eventSource = new EventSource("https://api.zeno.fm/mounts/metadata/subscribe/jhcuzjg99dquv");
+
+    eventSource.addEventListener("message", function(event) {
+        const data = JSON.parse(event.data);
+        if(data.streamTitle) {
+            currentSong.textContent = data.streamTitle;
+        };
+    });
+
+    eventSource.addEventListener("error", function(event){
+        console.error("Error in SSE connection", event);
+        currentSong.textContent = "Unable to fetch song title";
+    });
+  });
